543515ccf093cc813493e99d39ca4354cb41c996
Beto Quiroga <beto.m12@gmail.com>
2019-07-25 00:32:46 -0400

    Agrega Provider a nuestra app

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import "./styles/styles.scss"
+import store from './redux/store'
+import { Provider } from 'react-redux'
+import { getAllSpecialities } from './redux/actionCreators'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+store.dispatch(getAllSpecialities())
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
